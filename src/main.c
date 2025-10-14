f8801320ef4492ad8d28fbc9aa965e5ef761811c
Geffen Cooper <geffencooper@gmail.com>
2025-10-13 21:02:05 -0500

    5ms scan window

--- src/main.c
+++ src/main.c
@@ -165,8 +165,8 @@ static void scan_init(void)
 	 * devices that might update their advertising data at runtime. */
 	struct bt_le_scan_param scan_param = {
 		.type     = BT_LE_SCAN_TYPE_PASSIVE,
-		.interval = BT_GAP_SCAN_FAST_INTERVAL,
-		.window   = 0x0C,
+		.interval = 0x0080,
+		.window   = 0x0008,
 		.options  = BT_LE_SCAN_OPT_NONE
 	};
 
